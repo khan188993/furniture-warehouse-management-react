da39d24fa127c8582374b5caeafc7ff7c57564fa
Md Arfan Khan <khan188993@gmail.com>
2022-05-07 23:00:31 +0600

    5)handleAppProduct function added

--- src/pages/AddProduct/AddProduct.js
+++ src/pages/AddProduct/AddProduct.js
@@ -6,6 +6,10 @@ const AddProduct = () => {
     sold value initial 0, quantity input submit will get 
     
     */
+   const handleAddProduct = (e)=>{
+       e.preventDefault()
+       //here will add new product 
+   }
     return (
         <div className='w-50 mx-auto'>
             <h1 className='text-center my-4'>Add Product</h1>
@@ -13,7 +17,8 @@ const AddProduct = () => {
                 <form action="" onSubmit={handleAddProduct}>
                     product name: <input type="text" name='product_name' placeholder='product_name' /><br></br>
                     price : <input type="text" placeholder='price' name='price' /><br></br>
-                    desc : <input type="text" placeholder='desc' name='desc' />
+                    desc : <input type="text" placeholder='desc' name='desc' /><br></br>
+                    <input type="submit" value='submit' />
                 </form>
             </div>
         </div>
