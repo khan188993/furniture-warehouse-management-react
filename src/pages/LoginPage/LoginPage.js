9339a202a3bf1aa6359a2e6649e696596ad5f819
Md Arfan Khan <khan188993@gmail.com>
2022-05-07 23:15:24 +0600

    6)handle email password auth

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { useAuthState, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useAuthState, useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../Firebase/Firebase.init';
 
 
 const LoginPage = () => {
+    // const [email,setEmail] = useState("");
+    // const [password,setPassword] = useState("");
+    const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
     const navigate = useNavigate();
     const [user] = useAuthState(auth)
     const location = useLocation();
@@ -16,6 +19,17 @@ const LoginPage = () => {
         signInWithGoogle()
     }
 
+    //handling email password auth 
+    const handleEmailPassAuthLogin = (e)=>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        if(email && password){
+            signInWithEmailAndPassword(email, password);
+        }
+        /* login */
+    }
+
     if(user){
         navigate(redirectPath);
     }
@@ -25,6 +39,15 @@ const LoginPage = () => {
     return (
         <div>
             <h1>LoginPage</h1>
+            <div className="email-pass-login w-50 mx-auto">
+                <h1>Email Pass Login : </h1>
+                <form action="" onSubmit={handleEmailPassAuthLogin}>
+                    Email : <input type="email" name='email' placeholder='email' /><br/>
+                    Password: <input type="password" name='password' placeholder='password' /><br/>
+                    <input type="submit" value='submit' />
+                </form>
+            </div>
+            <hr />
             <button onClick={()=>handleGoogleAuth()} className='btn btn-danger'>Login witn google</button>
         </div>
     )
