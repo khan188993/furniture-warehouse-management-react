02a9a9e350a1948776734d2e6a2f6897865e6a60
Md Arfan Khan <khan188993@gmail.com>
2022-05-08 03:21:47 +0600

    11)product load and pass by useEffect custom hook

--- src/ContextApi/AppState.js
+++ src/ContextApi/AppState.js
@@ -1,13 +1,14 @@
 import React,{useState} from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../Firebase/Firebase.init';
+import useFetchProduct from '../hooks/useFetchProduct';
 import MyContext from './AppContext';
 
 const AppStates = (props) => {
-    const [userName, setUserName] = useState("arfan khan");
-
-
+    const {products,setProducts} = useFetchProduct('http://localhost:4000/furniture')
 
     return (
-        <MyContext.Provider value={{userName,setUserName}}>
+        <MyContext.Provider value={{products,setProducts}}>
             {props.children}
         </MyContext.Provider>
     )
